841fcf2e31e77995ea190d029c552231ced8a5c3
DimaSwiftYandex <otlichnik24@yandex.ru>
2023-10-16 20:23:37 +0300

    AlertPresenter and AlertModel are added

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Dmitry on 16.10.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Dmitry on 16.10.2023.
+//
+
+import Foundation
