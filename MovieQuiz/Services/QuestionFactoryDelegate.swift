07c2cd9af031795f88c5fb4b887420cadac46dc9
DimaSwiftYandex <otlichnik24@yandex.ru>
2023-10-16 19:52:12 +0300

    made protocols and delegates

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Dmitry on 16.10.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Dmitry on 16.10.2023.
+//
+
+import Foundation
