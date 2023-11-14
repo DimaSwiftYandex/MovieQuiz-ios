ee7266015a5c220f1ee35f1756640cc0a242f54c
DimaSwiftYandex <otlichnik24@yandex.ru>
2023-11-14 22:30:31 +0200

    Added code coverage with tests

---
+++ MovieQuiz/Services/StubNetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  StubNetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Dmitry on 13.11.2023.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Dmitry Dmitry on 12.11.2023.
+//
+
+import Foundation
